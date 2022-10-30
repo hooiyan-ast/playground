883495da3c692e541c7b12ab734fb0a652909575
hooiyan <hooiyan.hoe@atoz-software.tech>
2022-10-30 20:50:44 +0800

    update code

--- cra-react/src/App.js
+++ cra-react/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold capitalize">
+            {process.env.NODE_ENV} mode
+          </h1>
+          <p className="py-6 mb-8 font-bold text-green-500">
+            {process.env.NODE_ENV === 'production'
+              ? process.env.REACT_APP_PROD_API_URL
+              : process.env.REACT_APP_DEV_API_URL}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
